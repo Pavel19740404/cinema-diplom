4af9374d0345edb0c166088049640400455dd83e
Pavel19740404 <_email@example.com>
2026-07-08 18:54:29 +0300

    Add auth, admin seeder, is_admin field

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@cinema.ru',
+            'password' => Hash::make('admin123'),
+            'is_admin' => true,
+        ]);
+    }
+}
